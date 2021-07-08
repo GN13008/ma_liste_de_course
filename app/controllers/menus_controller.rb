5d4370c2b75565095b1b7840cd6103e5cb40ad98
Guillaume Negro <negro.guillaume@gmail.com>
2021-07-08 18:58:35 +0200

    first list version

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,5 +4,19 @@ class MenusController < ApplicationController
     @recettes = Recette.all.map {|recette| recette.name}
   end
   def liste
+  #   @rides = Ride.all
+    @recettes = params.values.first(14)
+    @liste = {}
+    @recettes.each do |recette|
+      unless recette == ""
+        Recette.where(name: recette).first.liste_ingredients.each do |liste_ingredient|
+          if @liste[liste_ingredient.ingredient.name].nil?
+            @liste[liste_ingredient.ingredient.name] = [liste_ingredient.quantity, liste_ingredient.unit]
+          else
+            @liste[liste_ingredient.ingredient.name][0] += liste_ingredient.quantity
+          end
+        end
+      end
+    end
   end
 end
